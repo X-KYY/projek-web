215c7de632eb9c9ab107bbaf215a48ce7453befb
KYY NEV <rizkyas800@gmail.com>
2025-09-03 21:45:19 +0700

    Add files via upload

---
+++ info.js
@@ -0,0 +1,12 @@
+// alert(`Permainan akan dimulai`)
+// const question = prompt(`Pilih antara batu, gunting, dan kertas`).toLowerCase();
+
+// let jawaban = [`batu`, `gunting`, `kertas`]
+// const random_jawaban = Math.floor(Math.random() * jawaban.length)
+// let random_lawan = jawaban[random_jawaban].toLowerCase()
+
+// if (question == random_lawan) {
+//     alert(`kamu Benar`)
+// } else {
+//     alert(`kamu salah, jawabannya adalah ${random_lawan}`)
+// }
